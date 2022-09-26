32051443f064f985167f6ede6f7ac081a1302969
Kerri Sharp <kerrisharp@intelligencefusion.co.uk>
2022-09-26 10:33:15 +0100

    Remove MicroDex dependency

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # go-hisui
+
+Search for and display information about a Pokémon.
+
+All Pokémon information is retrieved from https://pokeapi.co/
+
+# Credits
+
+- [PokeAPI](https://pokeapi.co/)
+- [MicroDex](https://github.com/404a10/MicroDex) for inspiration
--- main.go
+++ main.go
@@ -2,24 +2,70 @@ package main
 
 import (
 	"fmt"
-	"github.com/404a10/MicroDex"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/m7shapan/njson"
 )
+
+type Pokemon struct {
+	Name   string   `njson:"name"`
+	ID     int      `njson:"id"`
+	Height int      `njson:"height"`
+	Weight int      `njson:"weight"`
+	Type_1 string   `njson:"types.0.type.name"`
+	Type_2 string   `njson:"types.1.type.name"`
+	Moves  []string `njson:"moves.#.move.name"`
+}
 
 func main() {
 	var enteredPokemonName string
 
 	fmt.Println("Search for a Pokémon by name: ")
 	fmt.Scanf("%s", &enteredPokemonName)
-	show(microdex.CreatePokemon(enteredPokemonName))
+	show(CreatePokemon(enteredPokemonName))
 }
 
-func show(pokemon microdex.Pokemon) {
+func show(pokemon Pokemon) {
 	fmt.Println("Name:", pokemon.Name)
-	fmt.Println("ID:", pokemon.ID)
+	fmt.Println("National Pokédex number:", pokemon.ID)
+
 	if pokemon.Type_2 == "" {
 		fmt.Println("Type:", pokemon.Type_1)
 	} else {
 		fmt.Println("Type 1:", pokemon.Type_1)
 		fmt.Println("Type 2:", pokemon.Type_2)
 	}
+}
+
+func CreatePokemon(name string) Pokemon {
+	pokemonUrl := "https://pokeapi.co/api/v2/pokemon/"
+	req, _ := http.Get(pokemonUrl + strings.ToLower(name))
+
+	if req.StatusCode != 200 {
+		log.Fatalln("Pokémon not found. Please correct your search term and try again.")
+	}
+	defer req.Body.Close()
+
+	pokeJson := Pokemon{}
+
+	json, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal("An unexpected error occurred:", err)
+	}
+
+	if err := njson.Unmarshal(json, &pokeJson); err != nil {
+		fmt.Println(err)
+	}
+
+	pokeJson.Name = strings.Title(pokeJson.Name)
+	pokeJson.Type_1 = strings.Title(pokeJson.Type_1)
+	pokeJson.Type_2 = strings.Title(pokeJson.Type_2)
+	for i := 0; i < len(pokeJson.Moves); i++ {
+		pokeJson.Moves[i] = strings.Title(pokeJson.Moves[i])
+	}
+
+	return pokeJson
 }
