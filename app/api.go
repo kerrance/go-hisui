d9eab0d7036ba502d39148db94fdb18f159bd7bb
Kerri Sharp <kerrisharp@intelligencefusion.co.uk>
2023-03-17 11:31:23 +0000

    Shift API reading into own file

---
+++ app/api.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"fmt"
+	"github.com/kerrance/go-hisui/app/models"
+	"github.com/m7shapan/njson"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func CreatePokemon(enteredPokemonNameOrPokedexNumber string) models.Pokemon {
+	pokemonUrl := "https://pokeapi.co/api/v2/pokemon/"
+	req, _ := http.Get(pokemonUrl + strings.ToLower(enteredPokemonNameOrPokedexNumber))
+
+	if req.StatusCode != 200 {
+		log.Fatalln("Pokémon not found. Please correct your search term and try again.")
+	}
+	defer req.Body.Close()
+
+	json, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatal("An unexpected error occurred:", err)
+	}
+
+	pokemonJson := models.Pokemon{}
+
+	if err := njson.Unmarshal(json, &pokemonJson); err != nil {
+		fmt.Println(err)
+	}
+
+	return pokemonJson
+}
