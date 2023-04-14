99bb53f4ce3cf8c6123bd21244a6490e5f1eafdc
Kerri Sharp <kerrisharp@intelligencefusion.co.uk>
2023-04-14 11:38:20 +0100

    Sanitise a Pokemon's name for searching when entered by the user

--- app/exemptions.go
+++ app/exemptions.go
@@ -2,6 +2,26 @@ package app
 
 import "strings"
 
+func SanitisePokemonNameForSearch(pokemonName string) string {
+	switch strings.ToLower(pokemonName) {
+	case "farfetch'd":
+		return "farfetchd"
+	case "flabebé", "flabébe", "flabébé":
+		return "flabebe"
+	case "mimejr", "mimejr.", "mime jr.":
+		return "mime-jr"
+	case "mrmime", "mr.mime", "mr. mime":
+		return "mr-mime"
+	case "mrrime", "mr.rime", "mr. rime":
+		return "mr-rime"
+	case "sirfetch'd":
+		return "sirfetchd"
+	case "typenull", "type:null", "type: null":
+		return "type-null"
+	}
+	return pokemonName
+}
+
 func ShouldPokemonNameBeHyphenated(pokemonName string) bool {
 	switch strings.ToLower(pokemonName) {
 	case
