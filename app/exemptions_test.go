adcd74eed674840762e53067b024e97b7e1294ab
Kerri Sharp <kerrisharp@intelligencefusion.co.uk>
2023-04-14 16:10:06 +0100

    Add more tests for the Exemption methods

--- app/exemptions_test.go
+++ app/exemptions_test.go
@@ -5,6 +5,51 @@ import (
 	"testing"
 )
 
+func TestSanitisePokemonNameForSearch(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"Provide a Pokémon name with an apostrophe", "farfetch'd", "farfetchd"},
+		{"Provide a Pokémon name with a diacritic", "flabebé", "flabebe"},
+		{"Provide a Pokémon name with a diacritic", "flabébe", "flabebe"},
+		{"Provide a Pokémon name with a diacritic", "flabébé", "flabebe"},
+		{"Provide a Pokémon name with no space which needs one", "mimejr", "mime-jr"},
+		{"Provide a Pokémon name with a full-stop", "mimejr.", "mime-jr"},
+		{"Provide a Pokémon name with a space and a full stop", "mime jr.", "mime-jr"},
+		{"Provide a Pokémon name with no space which needs one", "mrmime", "mr-mime"},
+		{"Provide a Pokémon name with a full-stop", "mr.mime", "mr-mime"},
+		{"Provide a Pokémon name with a space and a full stop", "mr. mime", "mr-mime"},
+		{"Provide a Pokémon name with no space which needs one", "mrrime", "mr-rime"},
+		{"Provide a Pokémon name with a full-stop", "mr.rime", "mr-rime"},
+		{"Provide a Pokémon name with a space and a full stop", "mr. rime", "mr-rime"},
+		{"Provide a Pokémon name with an apostrophe", "sirfetch'd", "sirfetchd"},
+		{"Provide a Pokémon name with no space which needs one", "typenull", "type-null"},
+		{"Provide a Pokémon name with a colon", "type:null", "type-null"},
+		{"Provide a Pokémon name with a space and a colon", "type: null", "type-null"},
+		{"Provide a Pokémon name which should not be sanitised", "pikachu", "pikachu"},
+		{"Provide a Pokémon name which should not be sanitised", "CHARIZARD", "CHARIZARD"},
+		{"Provide a Pokémon name which should not be sanitised", "iron LEAVES", "iron LEAVES"},
+		{"Provide a Pokémon name which should not be sanitised", "mew two", "mew two"},
+		{"Provide a Pokémon name which should not be sanitised", "tapu-koko", "tapu-koko"},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(testing *testing.T) {
+			result := app.SanitisePokemonNameForSearch(testCase.input)
+			if result != testCase.expected {
+				testing.Errorf(
+					"Expected SanitisePokemonNameForSearch(%q) to be %v, but got %v",
+					testCase.input,
+					testCase.expected,
+					result,
+				)
+			}
+		})
+	}
+}
+
 func TestShouldPokemonNameBeHyphenated(t *testing.T) {
 	testCases := []struct {
 		name     string
@@ -35,6 +80,41 @@ func TestShouldPokemonNameBeHyphenated(t *testing.T) {
 			if result != testCase.expected {
 				testing.Errorf(
 					"Expected ShouldPokemonNameBeHyphenated(%q) to be %v, but got %v",
+					testCase.input,
+					testCase.expected,
+					result,
+				)
+			}
+		})
+	}
+}
+
+func TestDisplayPokemonNameWithPunctuationMarkIfNeeded(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{"Provide a Pokémon name which requires an apostrophe", "farfetchd", "farfetch'd"},
+		{"Provide a Pokémon name which requires a diacritic", "flabebe", "flabébé"},
+		{"Provide a Pokémon name which requires a space and a full stop", "mime-jr", "mime jr."},
+		{"Provide a Pokémon name which requires a space and a full stop", "mr-mime", "mr. mime"},
+		{"Provide a Pokémon name which requires a space and a full stop", "mr-rime", "mr. rime"},
+		{"Provide a Pokémon name which requires an apostrophe", "sirfetchd", "sirfetch'd"},
+		{"Provide a Pokémon name which requires a space and a colon", "type-null", "type: null"},
+		{"Provide a Pokémon name which should not be sanitised", "pikachu", "pikachu"},
+		{"Provide a Pokémon name which should not be sanitised", "CHARIZARD", "CHARIZARD"},
+		{"Provide a Pokémon name which should not be sanitised", "iron LEAVES", "iron LEAVES"},
+		{"Provide a Pokémon name which should not be sanitised", "mew two", "mew two"},
+		{"Provide a Pokémon name which should not be sanitised", "tapu-koko", "tapu-koko"},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(testing *testing.T) {
+			result := app.DisplayPokemonNameWithPunctuationMarkIfNeeded(testCase.input)
+			if result != testCase.expected {
+				testing.Errorf(
+					"Expected DisplayPokemonNameWithPunctuationMarkIfNeeded(%q) to be %v, but got %v",
 					testCase.input,
 					testCase.expected,
 					result,
